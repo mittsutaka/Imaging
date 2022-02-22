3f73ca96512b628570ff9bdd15e0db90f51e323a
mittsutaka <mitsutaka.murao@tryeting.jp>
2022-02-23 01:56:14 +0900

    最低限のボタンコンポーネント作成した

--- app/components/atoms/button/Button.tsx
+++ app/components/atoms/button/Button.tsx
@@ -1,15 +1,17 @@
+import { ButtonHTMLAttributes, MouseEventHandler } from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
   label: string;
   children?: JSX.Element | string;
   btnType?: string;
+  onClick?: MouseEventHandler;
 }
 
 export const Button = (props: ButtonProps) => {
-  const modeClass = props.btnType == "primary" ? "primary" : "secondory";
+  const modeClass = props.btnType == "primary" ? styles["btn-primary"] : styles["btn-secondory"];
   return (
-    <button className={[styles.btn, modeClass].join(" ")} type="button">
+    <button className={[styles.btn, modeClass].join(" ")} type='button' onClick={props.onClick}>
       {props.label}
     </button>
   );
